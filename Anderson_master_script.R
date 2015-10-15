364870bf9fd6401696c01978674b173e5b2768ae
-v <jhilgard@gmail.com>
2015-10-15 09:26:03 -0400

    More little edits. Moving to desktop.

--- Anderson_master_script.R
+++ Anderson_master_script.R
@@ -13,23 +13,23 @@ full_join(results2, results1) %>%
 
 # appraise similarities between SE and VAR as moderators
   # vs. 1/sqrt(N) and 1/N
-d1 = read.table("PETPEESE_output.txt", header=T)
-d2 = read.table("peters.txt", header=T)
-hist(d1$PET.b0 - d2$peters_linear.b0) # Very little difference
-hist(d1$PET.b1 - d2$peters_linear.b1) # Very little difference
-hist(d1$PEESE.b0 - d2$peters_quadratic.b0) # Very little difference
+# d1 = read.table("PETPEESE_output.txt", header=T)
+# d2 = read.table("peters.txt", header=T)
+# hist(d1$PET.b0 - d2$peters_linear.b0) # Very little difference
+# hist(d1$PET.b1 - d2$peters_linear.b1) # Very little difference
+# hist(d1$PEESE.b0 - d2$peters_quadratic.b0) # Very little difference
 
 # overlay
-plot(d1$PET.r, col="red", pch = 16, ylim = c(-.3, .5))
-points(d1$PET.r.LL, col="red")
-points(d1$PET.r.UL, col="red")
-points(d2$peters_linear.r, col="darkgreen", pch=16)
-points(d2$peters_linear.r.LL, col="darkgreen", cex=1.5)
-points(d2$peters_linear.r.UL, col="darkgreen", cex=1.5)
-# Similar results except for row 12.
-
-# For any reasonable N, my PET and PEESE should be
-  # giving essentially the same estimate b/c
-  # SE(z) was not a function of z.
-x = 10:200
-plot(1/sqrt(x), 1/sqrt(x-3))
+# plot(d1$PET.r, col="red", pch = 16, ylim = c(-.3, .5))
+# points(d1$PET.r.LL, col="red")
+# points(d1$PET.r.UL, col="red")
+# points(d2$peters_linear.r, col="darkgreen", pch=16)
+# points(d2$peters_linear.r.LL, col="darkgreen", cex=1.5)
+# points(d2$peters_linear.r.UL, col="darkgreen", cex=1.5)
+# # Similar results except for row 12.
+# 
+# # For any reasonable N, my PET and PEESE should be
+#   # giving essentially the same estimate b/c
+#   # SE(z) was not a function of z.
+# x = 10:200
+# plot(1/sqrt(x), 1/sqrt(x-3))
