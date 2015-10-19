a5838f16ed377388a5330ed97361eccff5ddf5b4
Joe Hilgard <jhilgard@asc.upenn.edu>
2015-10-19 12:22:16 -0400

    Separated funnel-plots to own script. - Added zero-centered, contour-enhanced plots.

---
+++ Anderson_plotting.R
@@ -0,0 +1,248 @@
+# Anderson plots
+dat = read.delim("cleaned_data.txt", stringsAsFactors=F)
+source("PETPEESE_functions.R")
+# Funnel plots centered at naive estimate, no contours
+# Affect ----
+windows(width = 8, height = 8)
+par(mfrow = c(2, 2))
+dat %>%
+  filter(Outcome == "AggAff", 
+         Setting == "Exp",
+         Best. %in% c("y", "n")) %>%
+  funnelPETPEESE(plotName = "All experiments", printText = F,
+                 xlim = c(-.5, 1.5), ylim = c(.32, 0))
+dat %>%
+  filter(Outcome == "AggAff", 
+         Setting == "Nonexp",
+         Best. %in% c("y", "n")) %>%
+  funnelPETPEESE(plotName = "All cross-sectional", printText = F, 
+                 xlim = c(-.1, .6), ylim = c(.1, 0))
+dat %>%
+  filter(Outcome == "AggAff", 
+         Setting == "Exp",
+         Best. %in% c("y")) %>%
+  funnelPETPEESE(plotName = "Best experiments", printText = F, 
+                 xlim = c(-.5, 1.5), ylim = c(.32, 0))
+dat %>%
+  filter(Outcome == "AggAff", 
+         Setting == "Nonexp",
+         Best. %in% c("y")) %>%
+  funnelPETPEESE(plotName = "Best cross-sectional", printText = F,
+                 xlim = c(-.1, .6), ylim = c(.1, 0))
+savePlot(filename="funnels_AggAff.pdf", type = "pdf")
+dev.off()
+
+# Behavior ----
+windows(width = 8, height = 8)
+par(mfrow = c(2, 2))
+dat %>%
+  filter(Outcome == "AggBeh", 
+         Setting == "Exp",
+         Best. %in% c("y", "n")) %>%
+  funnelPETPEESE(plotName = "All experiments", printText = F,
+                 xlim = c(-.4, .8), ylim = c(.3, 0))
+dat %>%
+  filter(Outcome == "AggBeh", 
+         Setting == "Nonexp",
+         Best. %in% c("y", "n")) %>%
+  funnelPETPEESE(plotName = "All cross-sectional", printText = F,
+                 xlim = c(-.2, .6), ylim = c(.185, 0))
+dat %>%
+  filter(Outcome == "AggBeh", 
+         Setting == "Exp",
+         Best. %in% c("y")) %>%
+  funnelPETPEESE(plotName = "Best experiments", printText = F,
+                 xlim = c(-.4, .8), ylim = c(.3, 0))
+dat %>%
+  filter(Outcome == "AggBeh", 
+         Setting == "Nonexp",
+         Best. %in% c("y")) %>%
+  funnelPETPEESE(plotName = "Best cross-sectional", printText = F,
+                 xlim = c(-.2, .6), ylim = c(.185, 0))
+savePlot(filename="funnels_AggBeh.pdf", type = "pdf")
+dev.off()
+
+# Cognition ----
+windows(width = 8, height = 8)
+par(mfrow = c(2, 2))
+dat %>%
+  filter(Outcome == "AggCog", 
+         Setting == "Exp",
+         Best. %in% c("y", "n")) %>%
+  funnelPETPEESE(plotName = "All experiments", printText = F,
+                 xlim = c(-.5, 1), ylim = c(.315, 0))
+dat %>%
+  filter(Outcome == "AggCog", 
+         Setting == "Nonexp",
+         Best. %in% c("y", "n")) %>%
+  funnelPETPEESE(plotName = "All cross-sectional", printText = F,
+                 xlim = c(-.1, .6), ylim = c(.125, 0))
+dat %>%
+  filter(Outcome == "AggCog", 
+         Setting == "Exp",
+         Best. %in% c("y")) %>%
+  funnelPETPEESE(plotName = "Best experiments", printText = F,
+                 xlim = c(-.5, 1), ylim = c(.315, 0))
+dat %>%
+  filter(Outcome == "AggCog", 
+         Setting == "Nonexp",
+         Best. %in% c("y")) %>%
+  funnelPETPEESE(plotName = "Best cross-sectional", printText = F,
+                 xlim = c(-.1, .6), ylim = c(.125, 0))
+savePlot(filename="funnels_AggCog.pdf", type = "pdf")
+dev.off()
+
+# Arousal ----
+windows(width = 4, height = 8)
+par(mfrow = c(2, 1))
+dat %>%
+  filter(Outcome == "PhysArous", 
+         Setting == "Exp",
+         Best. %in% c("y", "n")) %>%
+  funnelPETPEESE(plotName = "All experiments", printText = F,
+                 xlim = c(-.5, 1), ylim = c(.41, 0))
+dat %>%
+  filter(Outcome == "PhysArous", 
+         Setting == "Exp",
+         Best. %in% c("y")) %>%
+  funnelPETPEESE(plotName = "Best experiments", printText = F,
+                 xlim = c(-.5, 1), ylim = c(.41, 0))
+savePlot(filename="funnels_PhysArous.pdf", type = "pdf")
+dev.off()
+
+# Funnel plots centered at 0, significance contours
+# Affect ----
+windows(width = 8, height = 8)
+par(mfrow = c(2, 2))
+dat %>%
+  filter(Outcome == "AggAff", 
+         Setting == "Exp",
+         Best. %in% c("y", "n")) %>%
+  naive() %>% 
+  funnel(level=c(90, 95, 99), shade = c("white", "lightgrey", "grey"), refline=0,
+         main = "All experiments",
+         xlim = c(-.5, 1.5), ylim = c(.32, 0))
+dat %>%
+  filter(Outcome == "AggAff", 
+         Setting == "Nonexp",
+         Best. %in% c("y", "n")) %>%
+  naive() %>% 
+  funnel(level=c(90, 95, 99), shade = c("white", "lightgrey", "grey"), refline=0,
+         main = "All cross-sectional",
+         xlim = c(-.2, .6), ylim = c(.1, 0))
+dat %>%
+  filter(Outcome == "AggAff", 
+         Setting == "Exp",
+         Best. %in% c("y")) %>%
+  naive() %>% 
+  funnel(level=c(90, 95, 99), shade = c("white", "lightgrey", "grey"), refline=0,
+         main = "Best experiments",
+         xlim = c(-.5, 1.5), ylim = c(.32, 0))
+dat %>%
+  filter(Outcome == "AggAff", 
+         Setting == "Nonexp",
+         Best. %in% c("y")) %>%
+  naive() %>% 
+  funnel(level=c(90, 95, 99), shade = c("white", "lightgrey", "grey"), refline=0,
+         main = "Best cross-sectional",
+         xlim = c(-.2, .6), ylim = c(.1, 0))
+savePlot(filename="funnels-0_AggAff.pdf", type = "pdf")
+dev.off()
+
+# Behavior ----
+windows(width = 8, height = 8)
+par(mfrow = c(2, 2))
+dat %>%
+  filter(Outcome == "AggBeh", 
+         Setting == "Exp",
+         Best. %in% c("y", "n")) %>%
+  naive() %>% 
+  funnel(level=c(90, 95, 99), shade = c("white", "lightgrey", "grey"), refline=0,
+         main = "All experiments",
+         xlim = c(-.6, .6), ylim = c(.30, 0))
+dat %>%
+  filter(Outcome == "AggBeh", 
+         Setting == "Nonexp",
+         Best. %in% c("y", "n")) %>%
+  naive() %>% 
+  funnel(level=c(90, 95, 99), shade = c("white", "lightgrey", "grey"), refline=0,
+         main = "All cross-sectional",
+         xlim = c(-.4, .6), ylim = c(.19, 0))
+dat %>%
+  filter(Outcome == "AggBeh", 
+         Setting == "Exp",
+         Best. %in% c("y")) %>%
+  naive() %>% 
+  funnel(level=c(90, 95, 99), shade = c("white", "lightgrey", "grey"), refline=0,
+         main = "Best experiments",
+         xlim = c(-.6, .6), ylim = c(.30, 0))
+dat %>%
+  filter(Outcome == "AggBeh", 
+         Setting == "Nonexp",
+         Best. %in% c("y")) %>%
+  naive() %>% 
+  funnel(level=c(90, 95, 99), shade = c("white", "lightgrey", "grey"), refline=0,
+         main = "Best cross-sectional",
+         xlim = c(-.4, .6), ylim = c(.19, 0))
+savePlot(filename="funnels-0_AggBeh.pdf", type = "pdf")
+dev.off()
+
+# Cognition ----
+windows(width = 8, height = 8)
+par(mfrow = c(2, 2))
+dat %>%
+  filter(Outcome == "AggCog", 
+         Setting == "Exp",
+         Best. %in% c("y", "n")) %>%
+  naive() %>% 
+  funnel(level=c(90, 95, 99), shade = c("white", "lightgrey", "grey"), refline=0,
+         main = "All experiments",
+         xlim = c(-.2, .8), ylim = c(.315, 0))
+dat %>%
+  filter(Outcome == "AggCog", 
+         Setting == "Nonexp",
+         Best. %in% c("y", "n")) %>%
+  naive() %>% 
+  funnel(level=c(90, 95, 99), shade = c("white", "lightgrey", "grey"), refline=0,
+         main = "All cross-sectional",
+         xlim = c(-.4, .6), ylim = c(.125, 0))
+dat %>%
+  filter(Outcome == "AggCog", 
+         Setting == "Exp",
+         Best. %in% c("y")) %>%
+  naive() %>% 
+  funnel(level=c(90, 95, 99), shade = c("white", "lightgrey", "grey"), refline=0,
+         main = "Best experiments",
+         xlim = c(-.2, .8), ylim = c(.315, 0))
+dat %>%
+  filter(Outcome == "AggCog", 
+         Setting == "Nonexp",
+         Best. %in% c("y")) %>%
+  naive() %>% 
+  funnel(level=c(90, 95, 99), shade = c("white", "lightgrey", "grey"), refline=0,
+         main = "Best cross-sectional",
+         xlim = c(-.4, .6), ylim = c(.125, 0))
+savePlot(filename="funnels-0_AggCog.pdf", type = "pdf")
+dev.off()
+
+# Arousal ----
+windows(width = 4, height = 8)
+par(mfrow = c(2, 1))
+dat %>%
+  filter(Outcome == "PhysArous", 
+         Setting == "Exp",
+         Best. %in% c("y", "n")) %>%
+  naive() %>% 
+  funnel(level=c(90, 95, 99), shade = c("white", "lightgrey", "grey"), refline=0,
+         main = "All experiments",
+         xlim = c(-1, 1), ylim = c(.41, 0))
+dat %>%
+  filter(Outcome == "PhysArous", 
+         Setting == "Exp",
+         Best. %in% c("y")) %>%
+  naive() %>% 
+  funnel(level=c(90, 95, 99), shade = c("white", "lightgrey", "grey"), refline=0,
+         main = "Best experiments",
+         xlim = c(-1, 1), ylim = c(.41, 0))
+savePlot(filename="funnels-0_PhysArous.pdf", type = "pdf")
+dev.off()
