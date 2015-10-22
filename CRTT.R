394a60b836fa7d775a35e5662a21945fe329b9d9
-v <jhilgard@gmail.com>
2015-10-21 23:12:02 -0400

    More changes to CRTT coding and script.

--- CRTT.R
+++ CRTT.R
@@ -6,7 +6,7 @@ library(readxl)
 # Read in the data
 dat = read.delim("cleaned_data.txt", stringsAsFactors=F)
 dat2 = read_excel("CRTT.xlsx")
-dat = left_join(dat, dat2)
+dat = left_join(dat, dat2, by = c("Study.name", "Study", "Full.Reference"))
 
 dat.aggBeh = 
   dat %>% 
@@ -38,7 +38,20 @@ other.all
 
 funnel(model, 
        pch = ifelse(dat.aggBeh$CRTT %in% "Yes", 16, 1),
-       cex = ifelse(dat.aggBeh$CRTT %in% "Yes", 1, .7),
-       refline = 0, level = c(90, 95, 99))
+       cex = ifelse(dat.aggBeh$CRTT %in% "Yes", 1, .5),
+       refline = 0, level = c(90, 95, 99), shade = c("white", "gray95", "gray90"))
 
 tapply(dat.aggBeh$Sample.size, dat.aggBeh$CRTT, FUN = mean)
+
+egger.crtt.all = rma(yi = Fisher.s.Z, sei = Std.Err, mods = ~Std.Err,
+                     data = dat.aggBeh,
+                     subset = CRTT %in% "Yes")
+egger.other.all = rma(yi = Fisher.s.Z, sei = Std.Err, mods = ~Std.Err,
+                      data = dat.aggBeh,
+                      subset = !(CRTT %in% "Yes"))
+egger.crtt.all
+egger.other.all
+
+egger.crtt.all = rma(yi = Fisher.s.Z, sei = Std.Err, mods = ~Std.Err,
+                     data = dat.aggBeh,
+                     subset = CRTT %in% "Yes" & Study.name != "FRC08AB1e")
