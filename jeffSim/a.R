c3be036a48c79ccfc571bf2d4de47bf5196f2dd2
Joe Hilgard <jhilgard@gmail.com>
2016-03-07 20:20:08 -0500

    Finished SE of Z simulations.

--- jeffSim/a.R
+++ jeffSim/a.R
@@ -1,8 +1,8 @@
+# One way of looking at it
 
-M=10000
+M=1e4
 
-
-goSim=function(N,es)
+goSim=function(N, es)
 {
   d=1:M
   for (m in 1:M)
@@ -13,20 +13,20 @@ goSim=function(N,es)
     d[m]=(mean(y2)-mean(y1))/s
   }
   r = d / sqrt(d ^ 2 + 4)
-  z=0.5 * log((1 + r) / (1 - r))
+  z = 0.5 * log((1 + r) / (1 - r))
   return(c(mean(z),sd(z)))
 }
 
-myN=c(30,40,50,100,200,500)
+myN=c(30, 40, 50, 100, 200, 500)
 J=length(myN)
-out=matrix(ncol=2,nrow=J)
-for (j in 1:J) out[j,]=goSim(myN[j],0)
-out.2=matrix(ncol=2,nrow=J)
-for (j in 1:J) out.2[j,]=goSim(myN[j],.2)
-out.5=matrix(ncol=2,nrow=J)
-for (j in 1:J) out.5[j,]=goSim(myN[j],.5)
+out=matrix(ncol=2, nrow=J)
+for (j in 1:J) out[j, ] = goSim(myN[j], 0)
+out.2=matrix(ncol=2,nrow = J)
+for (j in 1:J) out.2[j, ] = goSim(myN[j], .2)
+out.5=matrix(ncol=2,nrow = J)
+for (j in 1:J) out.5[j, ] = goSim(myN[j], .5)
 
-pdf('sdCheck.pdf')
+png('seCheck1.png')
 tform=sqrt(1/(2*myN-3))
 plot(tform,out[,2],pch=19,col=rgb(1,0,0,.5),xlab="Standard Deviation of z from Borenstein Approximation",ylab="Standard Deviation of z (from simulation)")
 points(tform,out.2[,2],pch=19,col=rgb(0,1,0,.5))
@@ -62,20 +62,37 @@ dat$se.z_proper =  sqrt(term1.proper * term2)
 dat$se.z_improper = sqrt(term1.improper * term2)
 dat$se.z_approx = 1/sqrt(dat$n - 3)
 
+# Empirical estimator
+M = 1e4
+
+goSimDX = function(n1, n2, es)
+{
+  d=1:M
+  for (m in 1:M)
+  {
+    y1=rnorm(n1,0,1)
+    y2=rnorm(n2,es,1)
+    s=sqrt((var(y1)+var(y2))/2)
+    d[m]=(mean(y2)-mean(y1))/s
+  }
+  r = d / sqrt(d ^ 2 + 4)
+  z = 0.5 * log((1 + r) / (1 - r))
+  return(c(mean(z),sd(z)))
+}
+
+for (i in 1:nrow(dat)) {
+  temp = goSimDX(dat$n1[i], dat$n2[i], dat$delta[i])
+  dat$z_empirical[i] = temp[1]
+  dat$se.z_empirical[i] = temp[2]
+}
+
 # compare estimates
 library(ggplot2)
+library(dplyr)
 library(tidyr)
 
-ggplot(dat, aes(x = n, y = se.z_proper, 
-                shape = as.factor(ratio))) +
-  geom_point()
-
-ggplot(dat, aes(x = n, y = se.z_improper, 
-                shape = as.factor(ratio))) +
-  geom_point()
-
 output = dat %>% 
-  gather(se.z_proper:se.z_approx, key = "key", value = "SE_Z") %>% 
+  gather(se.z_proper:se.z_approx, se.z_empirical, key = "key", value = "SE_Z") %>% 
   separate(key, into = c("Stat", "Category"), sep = "_") %>% 
   filter(Category != "improper") 
 
@@ -83,20 +100,21 @@ output$delta.label = factor(output$delta,
                          levels=c(0, .25, .5), 
                          labels = c("delta = 0", "delta = 0.25", "delta = 0.5"))
 output$Estimator = factor(output$Category,
-                               levels = c("approx", "proper"),
-                               labels = c("1/sqrt(N-3)", "Pustej. eq'n 16"))
+                               levels = c("approx", "proper", "empirical"),
+                               labels = c("1/sqrt(N-3)", "Pustej. eq'n 16", "Empirical"))
 output$Allocation.ratio = factor(output$ratio)
-ggplot(output, aes(x = n, y = SE_Z, col = Category.label, shape = Allocation.ratio)) +
-  geom_point(position = position_jitter(width = 20)) +
+ggplot(output, aes(x = as.factor(n), y = SE_Z, col = Estimator, shape = Allocation.ratio)) +
+  geom_point(size = 4, position = position_jitter(width = .40)) +
   facet_wrap(~delta.label) +
   scale_y_continuous("Standard Error of Z") +
-  scale_x_continuous("Total sample size (jitter added for visibility)")
+  scale_x_discrete("Total sample size (jitter added for visibility)")
 
 summary(dat$se.z_proper - dat$se.z_approx) # difference does not exceed .009 in SE
+summary(dat$se.z_empirical - dat$se.z_approx) # 
 # Even in that most extreme case, the difference is about the same as N = 17 vs. N = 18.
 1/sqrt(18-3) - 1/sqrt(18-2) 
 
-ggsave("SE_Z_comparison.png")
+ggsave("./supplement/SE_Z_comparison.png")
 
 
 
